b13a1a62602cf7dc41f830f547ebb3e594bb94f0
Mohammed Faour <105264682+mfaourjc@users.noreply.github.com>
2022-07-14 14:48:25 +0300

    make email not required in deletedAccount

--- src/models/DeletedAccount.ts
+++ src/models/DeletedAccount.ts
@@ -2,7 +2,8 @@ import { Schema, model } from 'mongoose'
 
 const deletedAccountSchema = new Schema(
   {
-    email: { type: String, required: true },
+    id: { type: String, required: true },
+    email: { type: String, required: false },
     reason: { type: String, required: true },
     timespan: { type: String, required: true },
     matches: { type: Number, required: false, default: 0 },
