a42157f74e61ef39ccd4f247bedd6fc7465f92ef
Mohammed Faour <105264682+mfaourjc@users.noreply.github.com>
2022-07-18 23:00:48 +0300

    add match model

---
+++ src/models/Match.ts
@@ -0,0 +1,30 @@
+import { Schema, model, Types } from 'mongoose'
+import { Status } from '../types/Match'
+
+const userTonesSchema = new Schema(
+  {
+    userId: { Type: Types.ObjectId, required: true, ref: 'User' },
+    tone: {
+      type: String,
+      required: true,
+    },
+    reason: {
+      type: String,
+      required: true,
+    },
+    activity: { type: String, required: true },
+  },
+  { _id: false, timestamps: true },
+)
+
+const matchSchema = new Schema(
+  {
+    participants: { type: [Types.ObjectId], required: true, ref: 'User' },
+    status: { type: String, default: Status.pending, required: true, enum: Status },
+    rating: { type: Number, required: false },
+    userTones: { type: userTonesSchema, required: true },
+  },
+  { timestamps: true },
+)
+
+export const Match = model('Match', matchSchema)
---
+++ src/types/Match.ts
@@ -0,0 +1,5 @@
+export enum Status {
+  accepted = 'accepted',
+  rejected = 'rejected',
+  pending = 'pending',
+}
