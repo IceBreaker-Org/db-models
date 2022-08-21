ab93e5426c9fc037bcddedefb14c89ee9b32ae92
Mohammed Faour <105264682+mfaourjc@users.noreply.github.com>
2022-08-21 15:52:26 +0200

    add chatId to messages collection

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -5,6 +5,7 @@ const messageSchema = new Schema(
     sender: { type: Types.ObjectId, required: true, ref: 'User' },
     receiver: { type: Types.ObjectId, required: true, ref: 'User' },
     message: { type: String, required: true },
+    chatId: { type: Types.ObjectId, required: true, ref: 'Chat' },
   },
   { timestamps: true },
 )
