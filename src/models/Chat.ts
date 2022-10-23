80eca9aae09364874e50f663368076a18e0a7987
mohammed.faour <mohammed.faour@finn.auto>
2022-10-23 21:42:01 +0200

    add matchId to chats

--- src/models/Chat.ts
+++ src/models/Chat.ts
@@ -3,6 +3,7 @@ import { Schema, model, Types } from 'mongoose'
 const chatSchema = new Schema(
   {
     participants: { type: [Types.ObjectId], required: true, ref: 'User' },
+    matchId: { type: Types.ObjectId, require: true, ref: 'Match' },
     isActive: { type: Boolean, required: true, default: true },
     isSaved: { type: Boolean, required: true, default: false },
   },
