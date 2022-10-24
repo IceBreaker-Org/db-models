f4408f932435268ffbc4940ed02437320b204866
mohammed.faour <mohammed.faour@finn.auto>
2022-10-24 21:38:13 +0200

    add meetRequest model

---
+++ src/models/MeetRequest.ts
@@ -0,0 +1,22 @@
+import { Schema, model, Types } from 'mongoose'
+import { Status } from '../types/Match'
+
+const userSchema = new Schema(
+  {
+    user: { type: Types.ObjectId, required: true, ref: 'User' },
+    accepted: { type: Boolean, required: true },
+  },
+  { timestamps: false, _id: false },
+)
+
+const meetRequestSchema = new Schema(
+  {
+    initBy: { type: userSchema, required: true },
+    initTo: { type: userSchema, required: true },
+    matchId: { type: Types.ObjectId, required: true, ref: 'Match' },
+    status: { type: String, required: false, enum: Status, default: Status.pending },
+  },
+  { timestamps: true },
+)
+
+export const MeetRequest = model('MeetRequest', meetRequestSchema)
