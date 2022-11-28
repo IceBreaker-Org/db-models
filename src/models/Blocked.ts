b036e6f7e5d7a2cf33b351b55930d9ec66f9b3c9
mohammed.faour <mohammed.faour@finn.auto>
2022-11-28 23:42:31 +0100

    fix typo

--- src/models/Blocked.ts
+++ src/models/Blocked.ts
@@ -3,7 +3,7 @@ import { Schema, model, Types } from 'mongoose'
 const blockedSchema = new Schema(
   {
     blockedBy: { type: Types.ObjectId, required: true, ref: 'User' },
-    blokedUser: { type: Types.ObjectId, required: false, ref: 'User' },
+    blockedUser: { type: Types.ObjectId, required: false, ref: 'User' },
   },
   { timestamps: true },
 )
